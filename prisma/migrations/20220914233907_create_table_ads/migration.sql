1ac79d9ef75d1c7b1be496b020bb153fc7f23eff
Guilherme Athayde <guiathayde303@gmail.com>
2022-09-15 12:28:34 -0300

    Stage 3 finished

---
+++ prisma/migrations/20220914233907_create_table_ads/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - The primary key for the `Game` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- CreateTable
+CREATE TABLE "Ad" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "gameId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "yearsPlaying" INTEGER NOT NULL,
+    "discord" TEXT NOT NULL,
+    "weekDays" TEXT NOT NULL,
+    "hourStart" INTEGER NOT NULL,
+    "hourEnd" INTEGER NOT NULL,
+    "useVoiceChannel" BOOLEAN NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Ad_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Game" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "bannerUrl" TEXT NOT NULL
+);
+INSERT INTO "new_Game" ("bannerUrl", "id", "title") SELECT "bannerUrl", "id", "title" FROM "Game";
+DROP TABLE "Game";
+ALTER TABLE "new_Game" RENAME TO "Game";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
---
+++ src/utils/convertHoursStringToMinutes.ts
@@ -0,0 +1,5 @@
+export function convertHoursStringToMinutes(hourString: string) {
+  const [hour, minutes] = hourString.split(':').map(Number);
+  const timeInMinutes = hour * 60 + minutes;
+  return timeInMinutes;
+}
